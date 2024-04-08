77d32a27ecae525e273a79d597567d3d35b5b4d2
ndqkhanh20 <khanhndq2002@gmail.com>
2024-04-08 18:00:56 +0700

    [BE] - feat: build core-application-service

---
+++ core-service/core-domain/core-application-service/src/main/java/com/backend/programming/learning/system/domain/dto/create/CreateCertificateCourseCommand.java
@@ -0,0 +1,12 @@
+package com.backend.programming.learning.system.domain.dto.create;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CreateCertificateCourseCommand {
+}
