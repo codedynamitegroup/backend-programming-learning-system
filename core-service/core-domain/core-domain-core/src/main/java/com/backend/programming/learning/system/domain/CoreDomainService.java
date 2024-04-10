6a5726066b0cb6edab1a200e554dd4049fe42042
ndqkhanh20 <khanhndq2002@gmail.com>
2024-04-10 23:53:39 +0700

    [BE] - feat: add createCertificateCourse to CoreDomainService

--- core-service/core-domain/core-domain-core/src/main/java/com/backend/programming/learning/system/domain/CoreDomainService.java
+++ core-service/core-domain/core-domain-core/src/main/java/com/backend/programming/learning/system/domain/CoreDomainService.java
@@ -1,4 +1,8 @@
 package com.backend.programming.learning.system.domain;
 
+import com.backend.programming.learning.system.domain.entity.CertificateCourse;
+import com.backend.programming.learning.system.domain.event.CertificateCourseCreatedEvent;
+
 public interface CoreDomainService {
+    CertificateCourseCreatedEvent createCertificateCourse(CertificateCourse certificateCourse);
 }
--- core-service/core-domain/core-domain-core/src/main/java/com/backend/programming/learning/system/domain/CoreDomainServiceImpl.java
+++ core-service/core-domain/core-domain-core/src/main/java/com/backend/programming/learning/system/domain/CoreDomainServiceImpl.java
@@ -1,9 +1,16 @@
 package com.backend.programming.learning.system.domain;
 
+import com.backend.programming.learning.system.domain.entity.CertificateCourse;
+import com.backend.programming.learning.system.domain.event.CertificateCourseCreatedEvent;
 import lombok.extern.slf4j.Slf4j;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Slf4j
 public class CoreDomainServiceImpl implements CoreDomainService {
-
-
+    @Override
+    public CertificateCourseCreatedEvent createCertificateCourse(CertificateCourse certificateCourse) {
+        return new CertificateCourseCreatedEvent(certificateCourse, ZonedDateTime.now(ZoneId.of("Asis/Ho_Chi_Minh")));
+    }
 }
