e4b9575ecf03bbe7c066e3d61a63735418bd6919
ndqkhanh20 <khanhndq2002@gmail.com>
2024-04-15 12:45:21 +0700

    [BE] - feat: remove delete entity

--- core-service/core-domain/core-application-service/src/main/java/com/backend/programming/learning/system/core/service/domain/dto/create/certificatecourse_user/CreateCertificateCourseUserResponse.java
+++ core-service/core-domain/core-application-service/src/main/java/com/backend/programming/learning/system/core/service/domain/dto/create/certificatecourse_user/CreateCertificateCourseUserResponse.java
@@ -1,6 +1,5 @@
 package com.backend.programming.learning.system.core.service.domain.dto.create.certificatecourse_user;
 
-import com.backend.programming.learning.system.core.service.domain.entity.CertificateCourseTopic;
 import com.backend.programming.learning.system.core.service.domain.entity.CertificateCourseUser;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
