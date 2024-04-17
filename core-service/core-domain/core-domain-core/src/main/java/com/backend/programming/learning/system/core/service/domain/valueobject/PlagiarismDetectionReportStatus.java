41467ee69488ace2a363e2c261728a9a61356161
ndqkhanh20 <khanhndq2002@gmail.com>
2024-04-17 21:06:37 +0700

    [BE] - feat: add sql for report and calendar eventn

---
+++ core-service/core-domain/core-domain-core/src/main/java/com/backend/programming/learning/system/core/service/domain/valueobject/PlagiarismDetectionReportStatus.java
@@ -0,0 +1,8 @@
+package com.backend.programming.learning.system.core.service.domain.valueobject;
+
+public enum PlagiarismDetectionReportStatus {
+    PENDING,
+    PROCESSING,
+    COMPLETED,
+    FAILED
+}
