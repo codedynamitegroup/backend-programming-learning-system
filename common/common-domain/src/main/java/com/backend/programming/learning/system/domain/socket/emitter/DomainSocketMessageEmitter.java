25f3e6ab1b1b7d0c55ea0fcf3cfdc7352897fb03
ndqkhanh20 <khanhndq2002@gmail.com>
2024-04-19 20:15:16 +0700

    [BE] - feat: build socket io when create notification

--- common/common-domain/src/main/java/com/backend/programming/learning/system/domain/socket/emitter/DomainSocketMessageEmitter.java
+++
@@ -1,7 +0,0 @@
-package com.backend.programming.learning.system.domain.socket.emitter;
-
-import com.backend.programming.learning.system.domain.socket.DomainSocketMessage;
-
-public interface DomainSocketMessageEmitter<T extends DomainSocketMessage> {
-    void emit(T domainMessage);
-}
