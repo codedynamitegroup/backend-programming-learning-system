4623ec56950c98000bb422708d1a1bb8168eaccb
ndqkhanh20 <khanhndq2002@gmail.com>
2024-05-01 14:53:57 +0700

    [BE] - feat: resolve conflicts

--- core-service/core-domain/core-application-service/src/main/java/com/backend/programming/learning/system/core/service/domain/ports/output/message/publisher/user/UserDeletedSuccessMessagePublisher.java
+++
@@ -1,7 +0,0 @@
-package com.backend.programming.learning.system.core.service.domain.ports.output.message.publisher.user;
-
-import com.backend.programming.learning.system.core.service.domain.event.user.UserDeletedSuccessEvent;
-import com.backend.programming.learning.system.domain.event.publisher.DomainEventPublisher;
-
-public interface UserDeletedSuccessMessagePublisher extends DomainEventPublisher<UserDeletedSuccessEvent> {
-}
--- core-service/core-domain/core-application-service/src/main/java/com/backend/programming/learning/system/core/service/domain/ports/output/message/publisher/user/UserUpdatedSuccessMessagePublisher.java
+++
@@ -1,7 +0,0 @@
-package com.backend.programming.learning.system.core.service.domain.ports.output.message.publisher.user;
-
-import com.backend.programming.learning.system.core.service.domain.event.user.UserUpdatedSuccessEvent;
-import com.backend.programming.learning.system.domain.event.publisher.DomainEventPublisher;
-
-public interface UserUpdatedSuccessMessagePublisher extends DomainEventPublisher<UserUpdatedSuccessEvent> {
-}
