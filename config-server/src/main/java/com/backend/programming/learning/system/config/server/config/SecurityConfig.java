c6eb79768ab00419398f81634af563a1ff10dd0a
ndqkhanh20 <khanhndq2002@gmail.com>
2024-05-06 17:19:28 +0700

    [BE] - feat: add security for config server

--- config-server/src/main/java/com/backend/programming/learning/system/config/server/config/SecurityConfig.java
+++ config-server/src/main/java/com/backend/programming/learning/system/config/server/config/SecurityConfig.java
@@ -10,9 +10,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
-                .antMatchers("/actuator/**")
-                .antMatchers("/encrypt/**")
-                .antMatchers("/decrypt/**");
+                .antMatchers("/actuator/**");
         super.configure(web);
     }
 }
