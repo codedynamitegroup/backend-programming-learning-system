bd9e8b2c1f5becd5e2ee09b55d02f4f073cc73c1
ndqkhanh20 <khanhndq2002@gmail.com>
2024-05-06 16:36:10 +0700

    [BE] - feat: add actuator for config server

--- gateway-service/src/main/java/com/backend/programming/learning/system/gateway/service/config/WebSecurityConfig.java
+++ gateway-service/src/main/java/com/backend/programming/learning/system/gateway/service/config/WebSecurityConfig.java
@@ -2,11 +2,13 @@ package com.backend.programming.learning.system.gateway.service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public SecurityWebFilterChain webFluxSecurityConfig(ServerHttpSecurity httpSecurity) {
@@ -15,5 +17,12 @@ public class WebSecurityConfig {
                 .permitAll();
 //        httpSecurity.csrf().disable();
         return httpSecurity.build();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/actuator/**");
+        super.configure(web);
     }
 }
