c4b838a3c1755e732589a1a5710c5a0900979878
ndqkhanh20 <khanhndq2002@gmail.com>
2024-05-09 00:17:31 +0700

    [BE] - feat: add springdoc openapi for all services

--- core-service/core-container/src/main/java/com/backend/programming/learning/system/core/service/container/CoreServiceApplication.java
+++ core-service/core-container/src/main/java/com/backend/programming/learning/system/core/service/container/CoreServiceApplication.java
@@ -1,12 +1,18 @@
 package com.backend.programming.learning.system.core.service.container;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
+@EnableFeignClients
+@OpenAPIDefinition(info = @Info(
+        title = "Core Service API", version = "${springdoc.version}", description = "Documentation Core Service API v1.0"))
 @EnableJpaRepositories(basePackages = { "com.backend.programming.learning.system.core.service.dataaccess", "com.backend.programming.learning.system.dataaccess" })
 @EntityScan(basePackages = { "com.backend.programming.learning.system.core.service.dataaccess", "com.backend.programming.learning.system.dataaccess" })
 @SpringBootApplication(scanBasePackages = "com.backend.programming.learning.system")
--- course-service/course-container/src/main/java/com/backend/programming/learning/system/course/service/container/CourseServiceApplication.java
+++ course-service/course-container/src/main/java/com/backend/programming/learning/system/course/service/container/CourseServiceApplication.java
@@ -1,10 +1,18 @@
 package com.backend.programming.learning.system.course.service.container;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableDiscoveryClient
+@EnableFeignClients
+@OpenAPIDefinition(info = @Info(
+        title = "Course Service API", version = "${springdoc.version}", description = "Documentation Course Service API v1.0"))
 @EnableJpaRepositories(basePackages = {"com.backend.programming.learning.system.course.service.dataaccess", "com.backend.programming.learning.system.dataaccess"})
 @EntityScan(basePackages = {"com.backend.programming.learning.system.course.service.dataaccess", "com.backend.programming.learning.system.dataaccess"})
 @SpringBootApplication(scanBasePackages = "com.backend.programming.learning.system")
