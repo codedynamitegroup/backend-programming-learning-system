ade62e51b10c376d2a8398c0b944b15cce1c0fd2
ndqkhanh20 <khanhndq2002@gmail.com>
2024-05-08 22:19:52 +0700

    [BE] - feat: fix security to allow all

--- core-service/core-container/src/main/java/com/backend/programming/learning/system/core/service/container/config/WebSecurityConfig.java
+++ core-service/core-container/src/main/java/com/backend/programming/learning/system/core/service/container/config/WebSecurityConfig.java
@@ -3,8 +3,10 @@ package com.backend.programming.learning.system.core.service.container.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,12 +17,13 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/**").permitAll()
-                );
-
-        return http.build();
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/**").permitAll()
+                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 
 }
