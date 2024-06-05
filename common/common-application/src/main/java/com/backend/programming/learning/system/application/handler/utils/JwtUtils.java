7985897b9d1229719e300fd258ee08c5494ccefd
krnthoang542 <81230977+simplord542@users.noreply.github.com>
2024-06-05 15:06:14 +0700

    Update JwtUtils.java

--- common/common-application/src/main/java/com/backend/programming/learning/system/application/handler/utils/JwtUtils.java
+++ common/common-application/src/main/java/com/backend/programming/learning/system/application/handler/utils/JwtUtils.java
@@ -1,29 +1,27 @@
 package com.backend.programming.learning.system.application.handler.utils;
 
+import com.backend.programming.learning.system.application.handler.AuthException;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
 
 public class JwtUtils {
-    public static Map<String, String> getPayloadFromJwtString(String jwtString) {
+    public static JsonObject getPayloadFromJwtString(String jwtString) {
         if (jwtString == null || jwtString.isEmpty()) {
             return null;
         }
         String[] splitString = jwtString.split("\\.");
         Base64.Decoder decoder = Base64.getDecoder();
         String payload = new String(decoder.decode(splitString[1]));
-        String[] payloadSplit = payload.split(",");
-        Map<String, String> payloadMap = new HashMap<>();
-        for (String s : payloadSplit) {
-            String[] keyValue = s.split(":");
-            payloadMap.put(
-                    keyValue[0]
-                            .replace("\"", "")
-                            .replace("{", ""),
-                    keyValue[1].replace("\"", "")
-                            .replace("}", ""));
+        JsonObject convertedObject = null;
+
+        try {
+            convertedObject = new Gson().fromJson(payload, JsonObject.class);
+        } catch (Exception e) {
+            throw new AuthException("Error while converting jwt payload to json object");
         }
-        return payloadMap;
+        return convertedObject;
     }
 
     public static String getEmailFromJwtString(String jwtString) {
@@ -31,16 +29,27 @@ public class JwtUtils {
             return null;
         }
         String email = null;
-        Map<String, String> jwtPayloadMap = JwtUtils.getPayloadFromJwtString(jwtString);
-        if (jwtPayloadMap != null && jwtPayloadMap.containsKey("exp")) {
-            String exp = jwtPayloadMap.get("exp");
+        JsonObject jwtPayloadMap = getPayloadFromJwtString(jwtString);
+        if (jwtPayloadMap != null && jwtPayloadMap.has("exp")) {
+            String exp = jwtPayloadMap.get("exp").toString();
             if (Long.parseLong(exp) < System.currentTimeMillis() / 1000) {
                 return null;
             } else {
-                if (jwtPayloadMap.containsKey("preferred_username")) {
-                    email = jwtPayloadMap.get("preferred_username");
+                if (jwtPayloadMap.has("preferred_username")) {
+                    email = jwtPayloadMap.get("preferred_username").toString().replace("\"", "");
                 }
             }
+        }
+        return email;
+    }
+    public static String getEmailFromJwtStringWithoutCheckExp(String jwtString) {
+        if (jwtString == null || jwtString.isEmpty()) {
+            return null;
+        }
+        String email = null;
+        JsonObject jwtPayloadMap = getPayloadFromJwtString(jwtString);
+        if (jwtPayloadMap != null && jwtPayloadMap.has("preferred_username")) {
+            email = jwtPayloadMap.get("preferred_username").toString().replace("\"", "");
         }
         return email;
     }
