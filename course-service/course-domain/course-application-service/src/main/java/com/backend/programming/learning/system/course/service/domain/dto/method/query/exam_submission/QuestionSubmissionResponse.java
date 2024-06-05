5c8d4a8414f7883230738ee243be39d5c0d581bc
Đặng Ngọc Tiến <20127641@student.hcmus.edu.vn>
2024-06-05 22:21:50 +0700

    feat get detail exam submission

---
+++ course-service/course-domain/course-application-service/src/main/java/com/backend/programming/learning/system/course/service/domain/dto/method/query/exam_submission/QuestionSubmissionResponse.java
@@ -0,0 +1,24 @@
+package com.backend.programming.learning.system.course.service.domain.dto.method.query.exam_submission;
+
+import lombok.Builder;
+
+import java.util.UUID;
+
+/**
+ * com.backend.programming.learning.system.course.service.domain.dto.method.query.exam_submission
+ * Create by Dang Ngoc Tien
+ * Date 6/5/2024 - 9:45 PM
+ * Description: ...
+ */
+@Builder
+public record QuestionSubmissionResponse(
+        UUID questionId,
+        UUID examSubmissionId,
+        UUID userId,
+        Integer passStatus,
+        Float grade,
+        String content,
+        String rightAnswer,
+        Integer numFile
+) {
+}
---
+++ course-service/course-domain/course-application-service/src/main/java/com/backend/programming/learning/system/course/service/domain/implement/service/exam_submission/ExamSubmissionQueryHelper.java
@@ -0,0 +1,35 @@
+package com.backend.programming.learning.system.course.service.domain.implement.service.exam_submission;
+
+import com.backend.programming.learning.system.course.service.domain.dto.method.query.exam_submission.QueryExamSubmissionResponse;
+import com.backend.programming.learning.system.course.service.domain.entity.ExamSubmission;
+import com.backend.programming.learning.system.course.service.domain.entity.QuestionSubmission;
+import com.backend.programming.learning.system.course.service.domain.mapper.exam_submission.ExamSubmissionDataMapper;
+import com.backend.programming.learning.system.course.service.domain.ports.output.repository.ExamSubmissionRepository;
+import com.backend.programming.learning.system.course.service.domain.ports.output.repository.QuestionSubmissionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * com.backend.programming.learning.system.implement.exam_submission
+ * Create by Dang Ngoc Tien
+ * Date 4/24/2024 - 12:26 AM
+ * Description: ...
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ExamSubmissionQueryHelper {
+    private final ExamSubmissionRepository examSubmissionRepository;
+    private final QuestionSubmissionRepository questionSubmissionRepository;
+    private final ExamSubmissionDataMapper examSubmissionDataMapper;
+
+    public QueryExamSubmissionResponse submitExamDetail(UUID submissionId) {
+        ExamSubmission examSubmission = examSubmissionRepository.findBy(submissionId);
+        List<QuestionSubmission> questionSubmissions = questionSubmissionRepository.findAllByExamSubmissionId(submissionId);
+        return examSubmissionDataMapper.mapToQueryExamSubmissionResponse(examSubmission, questionSubmissions);
+    }
+}
