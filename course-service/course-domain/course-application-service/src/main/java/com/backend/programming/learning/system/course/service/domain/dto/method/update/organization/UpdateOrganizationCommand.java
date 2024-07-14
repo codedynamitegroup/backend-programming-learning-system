a0c033624ba25590991e6f67397318c1908e41a9
Dương Chí Thông <20127634@student.hcmus.edu.vn>
2024-07-14 12:11:35 +0700

    finish sync moodle

--- course-service/course-domain/course-application-service/src/main/java/com/backend/programming/learning/system/course/service/domain/dto/method/update/organization/UpdateOrganizationCommand.java
+++ course-service/course-domain/course-application-service/src/main/java/com/backend/programming/learning/system/course/service/domain/dto/method/update/organization/UpdateOrganizationCommand.java
@@ -5,23 +5,21 @@ import lombok.Builder;
 import lombok.Getter;
 
 import jakarta.validation.constraints.NotNull;
+
+import java.time.ZonedDateTime;
 import java.util.UUID;
 
 @Getter
 @Builder
 @AllArgsConstructor
 public class UpdateOrganizationCommand {
-    private final UUID organizationId;
-    @NotNull
     private final String description;
 
-    @NotNull
     private final String name;
 
-    @NotNull
     private final String apiKey;
 
-    @NotNull
     private final String moodleUrl;
+
 
 }
--- course-service/course-domain/course-application-service/src/main/java/com/backend/programming/learning/system/course/service/domain/implement/service/synchronize_state/SynchronizeStateApplicationServiceImpl.java
+++ course-service/course-domain/course-application-service/src/main/java/com/backend/programming/learning/system/course/service/domain/implement/service/synchronize_state/SynchronizeStateApplicationServiceImpl.java
@@ -1,8 +1,12 @@
 package com.backend.programming.learning.system.course.service.domain.implement.service.synchronize_state;
 
+import com.backend.programming.learning.system.course.service.domain.dto.method.query.synchronize_state.QuerySynchronizeStateCommand;
+import com.backend.programming.learning.system.course.service.domain.dto.method.query.synchronize_state.QuerySynchronizeStateResponse;
+import com.backend.programming.learning.system.course.service.domain.entity.Organization;
 import com.backend.programming.learning.system.course.service.domain.entity.SynchronizeState;
 import com.backend.programming.learning.system.course.service.domain.implement.service.moodle.MoodleCommandHandler;
 import com.backend.programming.learning.system.course.service.domain.ports.input.service.synchronize_state.SynchronizeStateApplicationService;
+import com.backend.programming.learning.system.course.service.domain.ports.output.repository.OrganizationRepository;
 import com.backend.programming.learning.system.course.service.domain.ports.output.repository.SynchronizeStateRepository;
 import com.backend.programming.learning.system.domain.valueobject.SynchronizeStatus;
 import com.backend.programming.learning.system.domain.valueobject.SynchronizeStep;
@@ -11,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -20,6 +25,44 @@ import java.util.UUID;
 public class SynchronizeStateApplicationServiceImpl implements SynchronizeStateApplicationService {
 
     private final MoodleCommandHandler moodleCommandHandler;
+    private final OrganizationRepository organizationRepository;
+    private final SynchronizeStateRepository synchronizeStateRepository;
+    private final SynchronizeStateCommandHandler synchronizeStateCommandHandler;
+
+
+    private Organization getOrganization(UUID organizationId) {
+        Optional<Organization> organization = organizationRepository.findOrganizationById(organizationId);
+        if (organization == null) {
+            log.error("Organization is not found");
+            throw new RuntimeException("Organization is not found");
+        }
+        log.info("Organization is found");
+        return organization.get();
+    }
+    @Override
+    public String syncDataMoodle(UUID organizationId) {
+        Organization organization = getOrganization(organizationId);
+        for(SynchronizeStep synchronizeStep : SynchronizeStep.values()){
+            SynchronizeState synchronizeState = SynchronizeState.builder().build();
+            synchronizeState.initializeSynchronizeState();
+            if(synchronizeStep.equals(SynchronizeStep.USER)) {
+                synchronizeState.setStatus(SynchronizeStatus.PROCESSING);
+            }
+            else {
+                synchronizeState.setStatus(SynchronizeStatus.PENDING);
+            }
+            synchronizeState.setOrganization(organization);
+            synchronizeState.setStep(synchronizeStep);
+            synchronizeStateRepository.save(synchronizeState);
+        }
+        return "Synchronization started";
+    }
+
+    @Override
+    public QuerySynchronizeStateResponse querySynchronizeByOrganizationIdAndStep(QuerySynchronizeStateCommand querySynchronizeStateCommand) {
+        return
+                synchronizeStateCommandHandler.querySynchronizeByOrganizationIdAndStep(querySynchronizeStateCommand.getOrganizationId(), querySynchronizeStateCommand.getStep());
+    }
 
     @Override
     public Boolean syncUser(UUID organizationId) {
