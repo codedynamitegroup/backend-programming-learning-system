9fef74a9511ec6660687cdf2d41da41cf3618b13
krnthoang542 <81230977+simplord542@users.noreply.github.com>
2024-07-18 18:22:25 +0700

    update data

--- course-service/course-container/src/main/resources/init-data.sql
+++ course-service/course-container/src/main/resources/init-data.sql
@@ -179,7 +179,7 @@ INSERT INTO public."module" (id, cmid, section_id, type_module, assignment_id, e
     ('8758ec8e-6322-4f6a-b68d-1c4a3c1e824a', 20, '70bb8281-5c4a-44a6-a745-0d72dd1abe20', 'ASSIGNMENT', '7055f5fd-29ce-48f9-b1d1-cd11a95ce931', null),
     ('caca48ad-21c5-414d-94c1-b324c25fe8c8', 22, '70bb8281-5c4a-44a6-a745-0d72dd1abe20', 'ASSIGNMENT', 'b3108f5a-c769-42ab-84c9-80a6d80f5821', null),
     ('2c8c1590-3e17-46bb-ac93-fb3d3b293544', 23, '70bb8281-5c4a-44a6-a745-0d72dd1abe20', 'ASSIGNMENT', '5b82b228-aef2-494f-8622-2be74966106d', null),
-    ('be09f62d-6d6f-4239-9b6b-08b784717788', 24, '7e89a8d4-5137-4457-81aa-1828265fc5ef', 'QUIZ', null, '86600cfb-7b48-4e81-8e05-8fa29d49d7a6');
+    ('be09f62d-6d6f-4239-9b6b-08b784717788', 24, '7e89a8d4-5137-4457-81aa-1828265fc5ef', 'QUIZ', null, '86600cfb-7b48-4e81-8e05-8fa29d49d7a9');
 
 
 INSERT INTO public.question(id, org_id, difficulty, name, question_text, general_feedback, default_mark, qtype, created_by, updated_by)
